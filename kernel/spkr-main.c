21257a2187144cde22e5319e57fb7781a916d3f0
Jorge Amorós <j1amoros944@gmail.com>
2018-01-08 17:38:28 +0100

    Update spkr-main.c

--- kernel/spkr-main.c
+++ kernel/spkr-main.c
@@ -2,6 +2,21 @@
 #include <linux/kernel.h>	/* Needed for KERN_INFO */
 #include <linux/init.h>		/* Needed for the macros */
 
+#include <linux/version.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include <linux/fs.h>
+#include <linux/wait.h>
+#include <linux/sched.h>
+#include <linux/kfifo.h>
+#include <linux/ioctl.h>
+#include <asm/uaccess.h>
+
+#include "version.h"
+#include "spkr-io.h"
+
+MODULE_LICENSE("Dual BSD/GPL");
+
 static int __init init_module(void)
 {
 	printk(KERN_INFO "Hello, world 2\n");
