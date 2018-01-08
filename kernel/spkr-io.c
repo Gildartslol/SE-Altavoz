ec7d247db06e94a296abfa43edc2898aaaea45a0
Jorge Amorós <j1amoros944@gmail.com>
2018-01-08 18:04:34 +0100

    Update spkr-io.c

--- kernel/spkr-io.c
+++ kernel/spkr-io.c
@@ -21,14 +21,10 @@ unsigned long spkrFlags;
 
 void set_spkr_frecuency(unsigned int frecuency){
 
-	uint8_t lCounter, hCounter;
 	unsigned long flags;
 	unsigned int freq = 1000;
 	if (frecuency > 20 && frecuency < 32767)
 		frecuency = PIT_TICK_RATE / frecuency;
-
-	//lCounter = (uint8_t) (freq & 0xff);
-	//hCounter = (uint8_t) (freq >> 8);
 	
 	outb_p(0xb6,SPKR_REGISTRO_CONTROL);
 
@@ -41,16 +37,16 @@ void set_spkr_frecuency(unsigned int frecuency){
 
 void spkr_on(void){
 
-	spin_lock_irqsave(&spkrSpinLock,spkrFlags)
+	spin_lock_irqsave(&spkrSpinLock,spkrFlags);
 	outb_p(inb_p(REGISTRO_CONTROL_SISTEMA) | 3, REGISTRO_CONTROL_SISTEMA);
-	spin_unlock_irqrestore(&spkrSpinLock,spkrFlags)
+	spin_unlock_irqrestore(&spkrSpinLock,spkrFlags);
 
 }
 
 
 void spkr_off(void){
 
-	spin_lock_irqsave(&spkrSpinLock,spkrFlags)
+	spin_lock_irqsave(&spkrSpinLock,spkrFlags);
 	outb(inb_p(REGISTRO_CONTROL_SISTEMA) & 0xFC, REGISTRO_CONTROL_SISTEMA);
-	spin_unlock_irqrestore(&spkrSpinLock,spkrFlags)
+	spin_unlock_irqrestore(&spkrSpinLock,spkrFlags);
 }
