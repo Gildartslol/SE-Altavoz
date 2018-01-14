2c8d7e318f85ea8c3f4ddf02cc9bbd863c04c933
Jorge Amorós <j1amoros944@gmail.com>
2018-01-14 17:13:07 +0100

    Update spkr-io.c

--- kernel/spkr-io.c
+++ kernel/spkr-io.c
@@ -24,8 +24,9 @@ void set_spkr_frecuency(unsigned int frecuency){
 	unsigned long flags;
 	unsigned int freq = 1000;
 	if (frecuency > 20 && frecuency < 32767)
-		freq = PIT_TICK_RATE / frecuency;
-
+		freq = PIT_TICK_RATE / frecuency;ç
+			
+	printk(KERN_INFO "PIT_TICK_RATE %s\n",PIT_TICK_RATE);	
 	printk(KERN_INFO "FRECUENCIA FIJADA EN %d\n",freq);	
 	
 	outb_p(0xb6,SPKR_REGISTRO_CONTROL);
