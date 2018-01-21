5312689eeada3dce47713fe16a145272e2d46ce4
Jorge Amorós <j1amoros944@gmail.com>
2018-01-21 01:26:07 +0100

    Update spkr-io.h

--- kernel/spkr-io.h
+++ kernel/spkr-io.h
@@ -1,3 +1,10 @@
 void set_spkr_frecuency(unsigned int frecuency);
 void spkr_on(void);
 void spkr_off(void);
+
+
+#if LINUX_VERSION_CODE == KERNEL_VERSION(3,0,0)
+	#define V3_0
+#elif LINUX_VERSION_CODE > KERNEL_VERSION(3,0,0)
+	#define V3_1
+#endif
